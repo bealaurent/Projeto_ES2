65135b9a326716c034d8b59ea1cae0d25d869507
Beathriz Muniz <59494495+bealaurent@users.noreply.github.com>
2025-01-26 11:50:25 -0300

    Update script_table.sql

--- script_table.sql
+++ script_table.sql
@@ -1,14 +1,15 @@
 CREATE TABLE issues (
     id SERIAL PRIMARY KEY,
     issue_id INT,
-    titulo TEXT,
-    descricao TEXT,
-    data_abertura TIMESTAMP,
-    data_conclusao TIMESTAMP,
-    tempo_resolucao INT,
-    prioridade TEXT,
-    milestone TEXT,
-    autor TEXT,
-    atribuido TEXT,
-    tema_relacionado TEXT
+    title TEXT,
+    body TEXT,
+    created_at TIMESTAMP,
+    closed_at TIMESTAMP,
+    user_author TEXT,
+    user_assigned TEXT,
+    labels TEXT[],
+    tema_relacionado TEXT,
+    resolution_time_days FLOAT,
+    priority TEXT,
+    milestone TEXT
 );
